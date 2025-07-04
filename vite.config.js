123c0a1d1b6b87e7a7790e63376e0926a9d1bfe7
Tharun2104 <tharunmogili09@gmail.com>
2025-07-03 21:42:34 -0400

    deploy commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Tharun-Portfolio/',
   plugins: [react()],
   server: {
     port: 3000,
@@ -12,4 +12,4 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true
   }
-})
+});
